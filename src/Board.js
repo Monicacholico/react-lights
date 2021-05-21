94ff71779d4161ca06a018685118cc9c26d2ea7d
Monicacholico <monicachol@hotmail>
2021-05-20 23:50:21 -0500

    starting to build components

--- src/Board.js
+++ src/Board.js
@@ -31,56 +31,88 @@ import './Board.css';
 
 class Board extends Component {
 
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  }
+
   constructor(props) {
     super(props);
 
-    // TODO: set initial state
-  }
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
 
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+  }
 
   createBoard() {
     let board = [];
-    // TODO: create array-of-arrays of true/false values
-    return board
-  }
-
-  /** handle changing a cell: update board & determine if winner */
-
-  flipCellsAround(coord) {
-    let {ncols, nrows} = this.props;
-    let board = this.state.board;
-    let [y, x] = coord.split("-").map(Number);
-
-
-    function flipCell(y, x) {
-      // if this coord is actually on board, flip it
-
-      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
-        board[y][x] = !board[y][x];
+    for( let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn);
       }
+      board.push(row);
     }
-
-    // TODO: flip this cell and the cells around it
-
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
-
-    this.setState({board, hasWon});
   }
-
 
   /** Render game board or winning message. */
 
   render() {
+    return (
 
-    // if the game is won, just show a winning msg & render nothing else
+      // if the game is won, just show a winning msg & render nothing else
 
-    // TODO
+      // TODO
+      <div className='Board'>
+        <table>
+          <tbody>
+            <tr>
+              <Cell isLit={true}/>
+              <Cell isLit={false}/>
+              <Cell isLit={true}/>
+              <Cell isLit={false}/>
+              <Cell isLit={true}/>
+            </tr>
+            <tr>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+            </tr>
+            <tr>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+            </tr>
+            <tr>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+            </tr>
+            <tr>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-    // make table board
+      // make table board
 
-    // TODO
+      // TODO
+    )
+
   }
 }
 
